76583197524ca5e127d7888d00ca551628d3e3cd
Jameson.Brown <jameson.brown@mx.com>
2020-08-09 17:41:50 -0600

    Initial Cleanup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="NavBar">
+        <h1>Kyler Oldroyd</h1>
+        <div className="NavButtons">
+          <button>My Gaming Website!</button>
+          <button>My Dog Website</button>
+          <button>About Me</button>
+        </div>
+      </div>
+      {/* Body */}
+      <div className="FootOfPage">
+
+      </div>
     </div>
   );
 }

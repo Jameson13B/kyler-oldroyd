c54b1c3951a70614c4d513b221af713b41d427e2
Jameson.Brown <jameson.brown@mx.com>
2020-08-10 10:21:00 -0600

    created home code tabs

---
+++ src/views/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.css'
+
+export const Home = () => {
+  return (
+    <div className="HomePage">
+      <h1>hello?</h1>
+      <h1>poop?</h1>
+    </div>
+  )
+}

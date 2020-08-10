dd07adb8e21d550dffeab9ddface90d848f41948
Jameson.Brown <jameson.brown@mx.com>
2020-08-10 16:12:26 -0600

    added description and pictures for home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Home.css'
+import GamingPic from "../assets/gaming_picture.jpg"
+import BlueJayPic from "../assets/blue_jay_picture.jpg"
 
 export const Home = () => {
   return (
     <div className="HomePage">
-      <h1>hello?</h1>
-      <h1>poop?</h1>
+      <h1 className="center">Hello! I am Kyler Oldroyd, and welcome to my Website!</h1>
+      <p className="center">Gaming, Birds and bird watching, Nature, and dogs!</p>
+      <div className="Pictures">
+        <img alt="gaming" src={GamingPic} />
+        <img alt="blue jay" src={BlueJayPic} />
+      </div>
     </div>
   )
 }
